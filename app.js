ea78f7b704843346428301f247cf3bbc1e996e8a
Anton <thetoxalike444@gmail.com>
2022-10-26 13:46:35 +0300

    new name class

--- app.js
+++ app.js
@@ -1,24 +1,20 @@
-const colorCircle = document.querySelector('.color__circle');
+const colorBox = document.querySelector('.color__box');
+const pickerColor = document.querySelector('.picker');
+const colorContainer = document.querySelector('.color__container');
 
 let colorStorage = []; 
 
 for (let i = 0; i <= 360; i++) {
     colorStorage.push(`hsl(${i - 5}, 100%, 50%)`);
-    // console.log(colorStorage);
 };
 
 colorStorage = colorStorage.toString();
-colorCircle.style.background = `conic-gradient(${colorStorage})`;
-
+colorBox.style.background = `conic-gradient(${colorStorage})`;
 
 // ---------------------------------------------------------------
 
-
-const pickerColor = document.querySelector('.picker');
-const colorContainer = document.querySelector('.color__container');
-
 pickerColor.onmousedown = (event) => {
-    event.preventDefault()
+    event.preventDefault();
 
     let shiftX = event.clientX - pickerColor.getBoundingClientRect().left;
     let shiftY = event.clientY - pickerColor.getBoundingClientRect().top;
@@ -27,32 +23,28 @@ pickerColor.onmousedown = (event) => {
     document.addEventListener('mouseup', onMouseUp);
 
     function onMouseMove(event) {
-        let newLeft = event.clientX - shiftX - colorCircle.getBoundingClientRect().left;
-        let newTop = event.clientY - shiftY - colorCircle.getBoundingClientRect().top
-        
+        let newLeft = event.clientX - shiftX - colorBox.getBoundingClientRect().left;
+        let newTop = event.clientY - shiftY - colorBox.getBoundingClientRect().top;
         if (newLeft < 0) {
             newLeft = 0;
         };
         if (newTop < 0) {
             newTop = 0;
         };
-        
-        let rightEdge =  colorCircle.offsetWidth - pickerColor.offsetWidth;
-        let topEdge = colorCircle.offsetHeight - pickerColor.offsetHeight;
-
+        let rightEdge = colorBox.offsetWidth - pickerColor.offsetWidth;
+        let topEdge = colorBox.offsetHeight - pickerColor.offsetHeight;
         if (newLeft > rightEdge) {
             newLeft = rightEdge;    
         };
         if (newTop > topEdge) {
             newTop = topEdge;
         };
-
         pickerColor.style.left = newLeft + 'px';
         pickerColor.style.top = newTop + 'px';    
     };
 
     function onMouseUp() {
-        document.removeEventListener('mouseup', onMouseUp)
+        document.removeEventListener('mouseup', onMouseUp);
         document.removeEventListener('mousemove', onMouseMove);
         pickerColor.onmousemove = false;
         pickerColor.onmouseup = false;
