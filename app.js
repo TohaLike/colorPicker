e780e2cf5dc19f86d0bcb88c1b1c742b8521d4dd
Anton <thetoxalike444@gmail.com>
2022-10-31 09:49:58 +0300

    reverted old changes

--- app.js
+++ app.js
@@ -1,15 +1,12 @@
 const colorBox = document.querySelector('.color__box');
-const pickerColor = document.querySelector('.picker');
+const pickerCursor = document.querySelector('.picker__cursor');
 const colorContainer = document.querySelector('.color__container');
+const colorIndex = document.querySelector('.color__index');
 const canvas = document.getElementById('colorCanvas');
 const context = canvas.getContext('2d', { willReadFrequently: true });   
    
 let width = colorBox.width;
 let height = colorBox.height;
-let positionX = 0;
-let positionY = 0;
-
-// context.rect(0, 0, width, height);
 let colorBackGround = context.createLinearGradient(0, 0, width, 0);
     colorBackGround.addColorStop(0, 'rgba(255, 0, 0, 1)');
     colorBackGround.addColorStop(0.17, 'rgba(255, 255, 0, 1)');
@@ -27,47 +24,57 @@ let colorBackGround = context.createLinearGradient(0, 0, width, 0);
     colorBackGround.addColorStop(1, 'rgba(0, 0, 0, 1)');
     context.fillStyle = colorBackGround;
     context.fillRect(0, 0, width, height);
-    context.fillRect(width, 0, 0, 0);
+    colorIndex.innerHTML = 'rgb(255, 255, 255)';
 
-// ---------------------------------------------------------------
-
-pickerColor.onmousedown = (event) => {
+pickerCursor.onmousedown = (event) => {
     event.preventDefault();
-    let shiftX = event.clientX - pickerColor.getBoundingClientRect().left;
-    let shiftY = event.clientY - pickerColor.getBoundingClientRect().top;
-        
+    let shiftX = event.clientX - pickerCursor.getBoundingClientRect().left;
+    let shiftY = event.clientY - pickerCursor.getBoundingClientRect().top;
+    let positionX = 0;
+    let positionY = 0;
+
     document.addEventListener('mousemove', onMouseMove);
     document.addEventListener('mousemove', getColorPicker);
     document.addEventListener('mouseup', onMouseUp);
-    
+
     function onMouseMove(event) { 
         let newLeft = event.clientX - shiftX - colorBox.getBoundingClientRect().left;
         let newTop = event.clientY - shiftY - colorBox.getBoundingClientRect().top;
         if (newLeft < 0) newLeft = 0;
         if (newTop < 0) newTop = 0;
-        let rightEdge = colorBox.offsetWidth - pickerColor.offsetWidth;
-        let topEdge = colorBox.offsetHeight - pickerColor.offsetHeight;
+        let rightEdge = colorBox.offsetWidth - pickerCursor.offsetWidth;
+        let topEdge = colorBox.offsetHeight - pickerCursor.offsetHeight;
         if (newLeft > rightEdge) newLeft = rightEdge;
         if (newTop > topEdge) newTop = topEdge;
-        pickerColor.style.left = newLeft + 'px';
-        pickerColor.style.top = newTop + 'px'; 
+        pickerCursor.style.left = newLeft + 'px';
+        pickerCursor.style.top = newTop + 'px';
         positionX = newLeft;
-        positionY = newTop;
+        positionY = newTop; 
     };
-       
+
     function getColorPicker() { 
         let imageData = context.getImageData(positionX, positionY, 1, 1).data;
         colorContainer.style.backgroundColor = `rgb(${imageData[0]}, ${imageData[1]}, ${imageData[2]})`; 
-        colorBox.style.backgroundColor = colorBackGround;
-    };
-    
+        colorIndex.innerHTML = `rgb(${imageData[0]}, ${imageData[1]}, ${imageData[2]})`; 
+        if (positionX === 0 && positionY === 0) {
+            colorContainer.style.backgroundColor = 'rgb(255, 255, 255)';
+            colorIndex.innerHTML = 'rgb(255, 255, 255)';
+        } else if (positionX === 325 && positionY === 0) {
+            colorContainer.style.backgroundColor = 'rgb(255, 255, 255)';
+            colorIndex.innerHTML = 'rgb(255, 255, 255)';
+        };
+        if (positionX === 0 && positionY === 325) {
+            colorContainer.style.backgroundColor = 'rgb(0, 0, 0)';
+            colorIndex.innerHTML = 'rgb(0, 0, 0)';
+        } else if (positionX === 325 && positionY === 325) {
+            colorContainer.style.backgroundColor = 'rgb(0, 0, 0)';
+            colorIndex.innerHTML = 'rgb(0, 0, 0)';
+        };  
+    }; 
     
     function onMouseUp() {
         document.removeEventListener('mouseup', onMouseUp);
         document.removeEventListener('mousemove', onMouseMove);
         document.removeEventListener('mousemove', getColorPicker);
-        pickerColor.onmousemove = false;
-        pickerColor.onmouseup = false;
     };
 };
-
