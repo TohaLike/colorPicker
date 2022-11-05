7e30be54f0a31bb1e672e1087a26108220c5bc4c
Anton Malyshev <90730699+TohaLike@users.noreply.github.com>
2022-11-05 23:59:24 +0300

    update

--- app.js
+++ app.js
@@ -7,7 +7,6 @@ const context = canvas.getContext('2d', { willReadFrequently: true });
 const rgbIndex = document.querySelector('.rgb__index'); 
 const hslIndex = document.querySelector('.hsl__index');
 const hexIndex = document.querySelector('.hex__index');
-
 const rgbR = document.getElementById('rgb__r');
 const rgbG = document.getElementById('rgb__g');
 const rgbB = document.getElementById('rgb__b');
@@ -15,14 +14,12 @@ const hexInputIndex = document.getElementById('hex__input__index');
 const hslH = document.getElementById('hsl__h');
 const hslS = document.getElementById('hsl__s');
 const hslL = document.getElementById('hsl__l');
-
 const inputRGB = document.querySelector('.color__search__rgb');
 const inputHEX = document.querySelector('.color__search__hex');
 const inputHSL = document.querySelector('.color__search__hsl');
 const typeColorRgb = document.querySelector('.type__color__rgb');
 const typeColorHex = document.querySelector('.type__color__hex');
 const typeColorHsl = document.querySelector('.type__color__hsl');
-
 const searchBtnRgb = document.querySelector('.search__btn__rgb');
 const searchBtnHex = document.querySelector('.search__btn__hex');
 const searchBtnHsl = document.querySelector('.search__btn__hsl');
@@ -54,13 +51,27 @@ let colorBackGround = context.createLinearGradient(0, 0, width, 0);
     hslIndex.innerHTML = 'hsl(0, 100%, 100%)';
     hexIndex.innerHTML = `#ffffff`;
 
+    pickerCursor.style.top = 0;
+    pickerCursor.style.left = 0;
+
+
 pickerCursor.onmousedown = (event) => {
-    event.preventDefault();
+    event.preventDefault();    
+    
+    canvas.addEventListener('mousedown', (e) => {
+        e.preventDefault()
+        canvas.addEventListener('mousemove', onMouseMove(e, pickerCursor));
+        document.addEventListener('mousedown', getColorPicker);
+        document.addEventListener('mousemove', getColorPicker);
+        document.addEventListener('mousemove', onMouseMove);
+        document.addEventListener('mouseup', onMouseUp);
+    });
+
     let shiftX = event.clientX - pickerCursor.getBoundingClientRect().left;
     let shiftY = event.clientY - pickerCursor.getBoundingClientRect().top;
     let positionX = 0;
-    let positionY = 0;
-
+    let positionY = 0;    
+    
     document.addEventListener('mousemove', onMouseMove);
     document.addEventListener('mousemove', getColorPicker);
     document.addEventListener('mouseup', onMouseUp);
@@ -103,8 +114,6 @@ pickerCursor.onmousedown = (event) => {
         rgbIndex.innerHTML = `rgb(${imageData[0]}, ${imageData[1]}, ${imageData[2]})`; 
         hexIndex.innerHTML = `#${hex(r)}${hex(g)}${hex(b)}`; 
         hslIndex.innerHTML = `hsl(${Math.round(h)}, ${Math.round(s)}%, ${Math.round(l)}%)`;
-        
-
         rgbR.value = `${imageData[0]}`;
         rgbG.value = `${imageData[1]}`;
         rgbB.value = `${imageData[2]}`;
@@ -112,26 +121,36 @@ pickerCursor.onmousedown = (event) => {
         hslH.value = `${Math.round(h)}`;
         hslS.value = `${Math.round(s)}`;
         hslL.value = `${Math.round(l)}`;
-
         if (positionX === 0 && positionY === 0) {
             colorResult.style.backgroundColor = 'rgb(255, 255, 255)';
             rgbIndex.innerHTML = 'rgb(255, 255, 255)';
             hexIndex.innerHTML = `#ffffff`;
+            rgbR.value = 255; rgbG.value = 255; rgbB.value = 255;
+            hexInputIndex.value = `#ffffff`;
         } else if (positionX === 325 && positionY === 0) {
             colorResult.style.backgroundColor = 'rgb(255, 255, 255)';
             rgbIndex.innerHTML = 'rgb(255, 255, 255)';
             hexIndex.innerHTML = `#ffffff`;
+            hexIndex.value = '#ffffff'
+            rgbR.value = 255; rgbG.value = 255; rgbB.value = 255;
+            hexInputIndex.value = `#ffffff`;
         };
         if (positionX === 0 && positionY === 325) {
             colorResult.style.backgroundColor = 'rgb(0, 0, 0)';
             rgbIndex.innerHTML = 'rgb(0, 0, 0)';
             hslIndex.innerHTML = 'hsl(0, 100%, 0%)';
-            hexIndex.innerHTML = `#000000`;
+            hexIndex.innerHTML = '#000000';
+            rgbR.value = 0; rgbG.value = 0; rgbB.value = 0;
+            hexInputIndex.value = `#000000`;
+            hslH.value = 0; hslS.value = 0; hslL.value = 0;
         } else if (positionX === 325 && positionY === 325) {
             colorResult.style.backgroundColor = 'rgb(0, 0, 0)';
             rgbIndex.innerHTML = 'rgb(0, 0, 0)';
             hslIndex.innerHTML = 'hsl(0, 100%, 0%)';
             hexIndex.innerHTML = `#000000`;
+            rgbR.value = 0; rgbG.value = 0; rgbB.value = 0;
+            hexInputIndex.value = '#000000';
+            hslH.value = 0; hslS.value = 0; hslL.value = 0;
         };  
     }; 
 
@@ -147,17 +166,15 @@ searchBtnRgb.addEventListener('click', () => {
     colorResult.style.backgroundColor = `rgb(${rgbR.value}, ${rgbG.value}, ${rgbB.value})`;
 });    
 typeColorRgb.addEventListener('click', () => {
-        if (inputHEX.style.display = 'none') {
-            inputRGB.style.display = 'none';
-            inputHEX.style.display = 'block';
-        };
-    });
-
+    if (inputHEX.style.display = 'none') {
+        inputRGB.style.display = 'none';
+        inputHEX.style.display = 'block';
+    };
+});
 
 searchBtnHex.addEventListener('click', () => {
     colorResult.style.backgroundColor = `${hexInputIndex.value}`;
 });
-
 typeColorHex.addEventListener('click', () => {
     if (inputHSL.style.display = 'none') {
         inputHEX.style.display = 'none';
