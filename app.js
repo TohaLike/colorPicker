850685786e8ffc221809ff1c1e343c47f4517ef7
Anton <thetoxalike444@gmail.com>
2022-11-15 09:18:44 +0300

    update

--- app.js
+++ app.js
@@ -186,22 +186,67 @@ searchBtnRgb.addEventListener('change', () => {
 
 let canvasRect = canvas.getBoundingClientRect();
 
-function colorToPocition(r, g, b) {
-    let [h, s, v] = RGBToHSL(r, g, b)
-    let hueX = Math.round(h);
-    let x = Math.round(s);
-    let y = Math.round(v);    
-    
-    colorCursorPosition(x, y, hueX);
 
-    document.addEventListener('change', getColorPicker);   
+function rgb2hsv(r, g, b) {
+    let rabs, gabs, babs, rr, gg, bb, h, s, v, diff, diffc, percentRoundFn;
+    rabs = r / 255;
+    gabs = g / 255;
+    babs = b / 255;
+    v = Math.max(rabs, gabs, babs),
+    diff = v - Math.min(rabs, gabs, babs);
+    diffc = c => (v - c) / 6 / diff + 1 / 2;
+    percentRoundFn = num => Math.round(num * 100) / 100;
+    if (diff == 0) {
+        h = s = 0;
+    } else {
+        s = diff / v;
+        rr = diffc(rabs);
+        gg = diffc(gabs);
+        bb = diffc(babs);
+
+        if (rabs === v) {
+            h = bb - gg;
+        } else if (gabs === v) {
+            h = (1 / 3) + rr - bb;
+        } else if (babs === v) {
+            h = (2 / 3) + gg - rr;
+        }
+        if (h < 0) {
+            h += 1;
+        }else if (h > 1) {
+            h -= 1;
+        }
+    }
+    return {
+        h: Math.round(h * 360),
+        s: percentRoundFn(s * 100),
+        v: percentRoundFn(v * 100)
+    };
+}
+
+
+function colorToPocition(r, g, b) {
+    rgb2hsv(r, g, b);
+ 
+    let x = b;
+    let y = g;
+    let hueX = r;
+    
+    colorCursorPosition(x, y);
+    colorHueCursor(hueX)
+
+    // document.addEventListener('change', getColorPicker);   
     document.addEventListener('change', removeSearchColor) 
     // removeSearchColor();
 
 };
 
-function colorCursorPosition(hueX, x, y) {
+
+function colorHueCursor(hueX) {
     pickerCursor.style.left = hueX + 'px';
+}
+
+function colorCursorPosition(x, y) {
     pickerCursor.style.left = x + 'px';
     pickerCursor.style.top = y + 'px';   
 };
